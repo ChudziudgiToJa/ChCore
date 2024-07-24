a7ddf2db7973b1495ec0a0a21de9b8328ef881d8
ChudziudgiToJa <Chudziudgi@gmail.com>
2024-07-24 20:56:20 +0200

    protection fix

--- src/main/java/pl/chudziudgi/core/feature/protection/ProtectionController.java
+++ src/main/java/pl/chudziudgi/core/feature/protection/ProtectionController.java
@@ -1,18 +1,25 @@
 package pl.chudziudgi.core.feature.protection;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
 import pl.chudziudgi.core.ChCore;
 import pl.chudziudgi.core.database.user.User;
 import pl.chudziudgi.core.database.user.UserManager;
+import pl.chudziudgi.core.feature.randomtp.RandomUtil;
 import pl.chudziudgi.core.util.TimeEnum;
+
+import java.util.Objects;
 
 public class ProtectionController implements Listener {
     private final ProtectionManager protectionManager;
@@ -33,6 +40,28 @@ public class ProtectionController implements Listener {
             if (protectionManager.hasProtection(player)) {
                 event.setCancelled(true);
             }
+        }
+    }
+
+    @EventHandler
+    public void onUseItem(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (!(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
+        if (!protectionManager.hasProtection(player)) return;
+        ItemStack item = event.getItem();
+        if (item != null && (item.getType() == Material.BOW ||
+                item.getType() == Material.FISHING_ROD ||
+                item.getType() == Material.CROSSBOW ||
+                item.getType() == Material.TRIDENT)) {
+            event.setCancelled(true);
+            player.setCooldown(event.getMaterial(), 5);
+        }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if (!UserManager.isExists(event.getPlayer())) {
+            this.protectionManager.giveProtection(event.getPlayer(), TimeEnum.MINUTE, 3);
         }
     }
 
